f933a98b1343f38c600a1bff6bff87dae0318efb
OnceJR <131706454+OnceJR@users.noreply.github.com>
2024-10-29 23:49:30 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,12 +20,16 @@ bot = TelegramClient('my_bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Inicializa el navegador
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-software-rasterizer")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-disk-cache")
+
+# Inicia el navegador con las opciones configuradas
+driver = webdriver.Chrome(options=chrome_options)
 
 LINKS_FILE = 'links.json'
 DOWNLOAD_PATH = "/root/cbautorec/"
