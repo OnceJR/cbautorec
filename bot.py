506325a56c2633a90caf910ecc74188a12c5a16e
OnceJR <131706454+OnceJR@users.noreply.github.com>
2024-11-01 03:40:09 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -139,13 +139,10 @@ async def download_with_yt_dlp(m3u8_url, user_id):
         logging.info(f"Iniciando descarga con yt-dlp: {m3u8_url}")
         await bot.send_message(int(user_id), f"🔴 Iniciando grabación para el enlace: {m3u8_url}")  # Convierte user_id a entero
         
-        # Crear el proceso asíncrono
         process = await asyncio.create_subprocess_exec(*command_yt_dlp, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-        await process.wait()  # Esperar a que termine el proceso
+        await process.wait()
         
         logging.info("Descarga completa.")
-
-        # Llamada a la función de subida y eliminación
         await upload_and_delete_mp4_files(user_id)
         
     except Exception as e:
